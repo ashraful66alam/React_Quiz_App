ccdda5dcb4708fd9de4945662ac56c9f72854e89
ashraful66alam <aamorroid66@gmail.com>
2024-05-23 18:01:23 +0600

    Routing done

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,22 +1,59 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "../styles/App.css";
 import Layout from "./Layout";
+import HomePage from "./Pages/HomePage";
+import Login from "./Pages/Login";
+import Quiz from "./Pages/Quiz";
 import Result from "./Pages/Result";
-// import Login from "./Pages/Login";
-// import Quiz from "./Pages/Quiz";
-// import HomePage from "./Pages/HomePage";
-// import Signup from "./Pages/Signup";
+import Signup from "./Pages/Signup";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <HomePage />
+      </Layout>
+    ),
+  },
+  {
+    path: "signup",
+    element: (
+      <Layout>
+        <Signup />
+      </Layout>
+    ),
+  },
+  {
+    path: "login",
+    element: (
+      <Layout>
+        <Login />
+      </Layout>
+    ),
+  },
+  {
+    path: "quiz",
+    element: (
+      <Layout>
+        <Quiz />
+      </Layout>
+    ),
+  },
+  {
+    path: "result",
+    element: (
+      <Layout>
+        <Result />
+      </Layout>
+    ),
+  },
+]);
 function App() {
   return (
     <>
       <div className="App">
-        <Layout>
-          {/* <HomePage /> */}
-          {/* <Signup /> */}
-          {/* <Login /> */}
-          {/* <Quiz></Quiz> */}
-          <Result></Result>
-        </Layout>
+        <RouterProvider router={router} />;
       </div>
     </>
   );
